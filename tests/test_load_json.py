3eeed7b8728b0de166f0b64568856c1d39438514
Michael Wood-Vasey <wmwv@pitt.edu>
2017-07-28 11:47:23 -0400

    Test plots are being generated with expected name.
    Just checks check_astrometry.png right now.

--- tests/test_load_json.py
+++ tests/test_load_json.py
@@ -31,7 +31,8 @@ import lsst.utils
 
 from lsst.validate.base import load_metrics
 from lsst.validate.drp.validate import (
-    get_filter_name_from_job, load_json_output, print_metrics)
+    get_filter_name_from_job, load_json_output, plot_metrics, print_metrics)
+from lsst.validate.drp.util import repoNameToPrefix
 
 
 class ParseJsonJob(unittest.TestCase):
@@ -81,6 +82,24 @@ class ParseJsonJob(unittest.TestCase):
         filterName = get_filter_name_from_job(job)
         metrics = {meas.metric.name: meas.metric for meas in job.measurements}
         print_metrics(job, filterName, metrics)
+
+    def testPlotMetricsFromJsonJob(self):
+        """Does plotting the metrics run and produce the correct filenames?
+
+        This could be loosely seen as a test of the outputPrefix handling
+        and thus DM-11410, it's a rather incomplete one because the chain
+        of wrapping functions is different.
+        """
+        job = load_json_output(self.jsonFile)
+        filterName = get_filter_name_from_job(job)
+
+        plot_metrics(job, filterName)
+        assert os.path.exists('check_astrometry.png')
+
+        outputPrefix = repoNameToPrefix(self.jsonFile)
+        plot_metrics(job, filterName, outputPrefix=outputPrefix)
+        expectedCheckAstrometryFile = outputPrefix+'check_astrometry.png'
+        assert os.path.exists(expectedCheckAstrometryFile)
 
 
 def setup_module(module):
